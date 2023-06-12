54821de42fd86187469865d3239869e47b836bd5
AgaZthi <agasthi@zoomi.ca>
2023-06-12 14:54:28 +0530

    working

--- test2/src/app/app.component.ts
+++ test2/src/app/app.component.ts
@@ -11,6 +11,8 @@ export class AppComponent {
   isPlaying: boolean = false;
   currentTime: number = 0;
   duration: number = 0;
+  cutMarkStart: number | null = null;
+  cutMarkEnd: number | null = null;
 
   togglePlayPause() {
     const videoPlayer = this.videoPlayerRef.nativeElement;
@@ -22,13 +24,43 @@ export class AppComponent {
       videoPlayer.pause();
     }
   }
-
-  ngAfterViewInit() {
+  updateProgress() {
     const videoPlayer = this.videoPlayerRef.nativeElement;
+    this.currentTime = videoPlayer.currentTime;
+    this.duration = videoPlayer.duration;
 
-    videoPlayer.addEventListener('timeupdate', () => {
-      this.currentTime = videoPlayer.currentTime;
-      this.duration = videoPlayer.duration;
-    });
+    if (this.cutMarkStart !== null && this.cutMarkEnd !== null) {
+      if (
+        this.currentTime < this.cutMarkStart ||
+        this.currentTime > this.cutMarkEnd
+      ) {
+        videoPlayer.currentTime = this.cutMarkStart;
+      }
+    }
+  }
+
+  setCutMark() {
+    const videoPlayer = this.videoPlayerRef.nativeElement;
+    if (this.cutMarkStart === null) {
+      this.cutMarkStart = videoPlayer.currentTime;
+    } else if (this.cutMarkEnd === null) {
+      this.cutMarkEnd = videoPlayer.currentTime;
+    }
+  }
+
+  trimVideo() {
+    if (this.cutMarkStart !== null && this.cutMarkEnd !== null) {
+      // Implement logic to trim the video using the cutMarkStart and cutMarkEnd values
+      console.log(
+        'Video trimmed from',
+        this.cutMarkStart,
+        'to',
+        this.cutMarkEnd
+      );
+      this.cutMarkStart = null;
+      this.cutMarkEnd = null;
+    } else {
+      console.log('Please set both start and end cut marks before trimming.');
+    }
   }
 }
